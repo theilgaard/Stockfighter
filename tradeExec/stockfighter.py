cf88099e96ae69b7a61889cb8c88846587d55994
Kristian Theilgaard <kristian.theilgaard@gmail.com>
2015-12-23 23:18:06 +0100

    Fixes headers and json encoding of body in stockfigther client

--- tradeExec/stockfighter.py
+++ tradeExec/stockfighter.py
@@ -4,23 +4,23 @@ import json
 class StockFighter:
     def __init__(self, apikey):
         self.headers = {'X-Starfighter-Authorization':apikey}
-        self.h = httplib2.Http(".cache")
+        self.http = httplib2.Http(".cache")
 
     def isApiUp(self):
         url = "https://api.stockfighter.io/ob/api/heartbeat"
-        (resp_headers, content) = self.h.request(url, "GET")
+        (resp_headers, content) = self.http.request(url, "GET")
         health = json.loads(content.decode('UTF-8'))
         return health['ok']
 
     def isVenueUp(self, venue):
         url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/heartbeat"
-        (resp_headers, content) = self.h.request(url, "GET")
+        (resp_headers, content) = self.http.request(url, "GET")
         health = json.loads(content.decode('UTF-8'))
         return health['ok']
 
     def stockOnVenue(self, venue):
         url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks"
-        (resp_headers, content) = self.h.request(url, "GET")
+        (resp_headers, content) = self.http.request(url, "GET")
         stocks = json.loads(content.decode('UTF-8'))
         if stocks['ok']:
             return stocks['symbols']
@@ -29,7 +29,7 @@ class StockFighter:
 
     def getOrderbook(self, venue, stock):
         url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks/"+stock 
-        (resp_headers, content) = self.h.request(url, "GET")
+        (resp_headers, content) = self.http.request(url, "GET")
         orderbook = json.loads(content.decode('UTF-8'))
         if orderbook['ok']:
             return orderbook
@@ -38,16 +38,16 @@ class StockFighter:
 
     def placeOrder(self, order):
         url = "https://api.stockfighter.io/ob/api/venues/"+order['venue']+"/stocks/"+order['symbol']+"/orders"
-        (resp_headers, content) = self.h.request(url, "POST", self.headers)
-        order = json.loads(content.decode('UTF-8'))
-        if order['ok']:
-            return order
+        (resp_headers, content) = self.http.request(url, "POST", headers=self.headers, body=json.dumps(order))
+        orderreply = json.loads(content.decode('UTF-8'))
+        if orderreply['ok']:
+            return orderreply
         else: 
-            print("[!] Error placing order: " + order['error'])
+            print("[!] Error placing order: " + orderreply['error'])
 
     def getQuote(self, venue, stock):
         url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks/"+stock+"/quote"
-        (resp_headers, content) = self.h.request(url, "GET")
+        (resp_headers, content) = self.http.request(url, "GET")
         quote = json.loads(content.decode('UTF-8'))
         if quote['ok']:
             return quote
@@ -56,7 +56,7 @@ class StockFighter:
 
     def getOrderStatus(self, venue, stock, orderid):
         url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks/"+stock+"/orders/"+orderid
-        (resp_headers, content) = self.h.request(url, "GET")
+        (resp_headers, content) = self.http.request(url, "GET")
         status = json.loads(content.decode('UTF-8'))
         if status['ok']:
             return status
@@ -65,7 +65,7 @@ class StockFighter:
 
     def cancelOrder(self, venue, stock, orderid):
         url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks/"+stock+"/orders/"+orderid
-        (resp_headers, content) = self.h.request(url, "DELETE", self.headers)
+        (resp_headers, content) = self.http.request(url, "DELETE", headers=self.headers)
         response = json.loads(content.decode('UTF-8'))
         if response['ok']:
             return response
@@ -78,7 +78,7 @@ class StockFighter:
             url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/accounts/"+account+"/orders"
         else:
             url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/accounts/"+account+"/stocks/"+stock+"/orders"
-        (resp_headers, content) = self.h.request(url, "GET", self.headers)
+        (resp_headers, content) = self.http.request(url, "GET", headers=self.headers)
         orders = json.loads(content.decode('UTF-8'))
         if orders['ok']:
             return orders
