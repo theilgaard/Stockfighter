5afccdd12238ade8b0592b0b4301088bc052a43e
Kristian Theilgaard <kristian.theilgaard@gmail.com>
2015-12-23 19:37:18 +0100

    Add methods for all API endpoints

--- tradeExec/stockfighter.py
+++ tradeExec/stockfighter.py
@@ -36,3 +36,52 @@ class StockFighter:
         else:
             print("[!] Error retrieving orderbook: " + orderbook['error'])
 
+    def placeOrder(self, order):
+        url = "https://api.stockfighter.io/ob/api/venues/"+order['venue']"/stocks/"+order['stock']+"/orders"
+        (resp_headers, content) = self.h.request(url, "POST", self.headers)
+        order = json.loads(content.decode('UTF-8'))
+        if order['ok']:
+            return order
+        else: 
+            print("[!] Error placing order: " + order['error'])
+
+    def getQuote(self, venue, stock):
+        url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks/"+stock+"/quote"
+        (resp_headers, content) = self.h.request(url, "GET")
+        quote = json.loads(content.decode('UTF-8'))
+        if quote['ok']:
+            return quote
+        else:
+            print("[!] Error retrieving quote for stock: " + quote['error'])
+
+    def getOrderStatus(self, venue, stock, orderid):
+        url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks/"+stock+"/orders/"+orderid
+        (resp_headers, content) = self.h.request(url, "GET")
+        status = json.loads(content.decode('UTF-8'))
+        if status['ok']:
+            return status
+        else:
+            print("[!] Error retrieving status for order: " + status['error'])
+
+    def cancelOrder(self, venue, stock, orderid):
+        url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/stocks/"+stock+"/orders/"+orderid
+        (resp_headers, content) = self.h.request(url, "DELETE", self.headers)
+        response = json.loads(content.decode('UTF-8'))
+        if response['ok']:
+            return response
+        else:
+            print("[!] Error cancelling order: " + response['error'])
+
+    def getOrders(self, venue, account, stock=None):
+        url = ''
+        if stock is None:
+            url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/accounts/"+account+"/orders"
+        else:
+            url = "https://api.stockfighter.io/ob/api/venues/"+venue+"/accounts/"+account+"/stocks/"+stock+"/orders"
+        (resp_headers, content) = self.h.request(url, "GET", self.headers)
+        orders = json.loads(content.decode('UTF-8'))
+        if orders['ok']:
+            return orders
+        else:
+            print("[!] Error retrieving orders: " + orders['error'])
+
